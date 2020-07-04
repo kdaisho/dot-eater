ac8ebaacd70c4f3467b00cc9714412d8f464d0db
daisho <daishokomiyama@gmail.com>
2020-07-04 16:50:59 -0400

    Adding enemy move

--- js/app.js
+++ js/app.js
@@ -6,6 +6,20 @@ const cell = {
 	maxHeight: 8
 };
 
+const direction = {
+	up: 0,
+	left: 1,
+	down: 2,
+	right: 3
+};
+
+const directions = [
+	new Vec2(0, -1),
+	new Vec2(-1, 0),
+	new Vec2(0, 1),
+	new Vec2(1, 0)
+];
+
 const cells = [
 	[1,1,1,1,0,1,1,1,1],
 	[1,0,0,0,0,0,0,0,1],
@@ -42,22 +56,27 @@ function Character(aa) {
 
 const characters = [
 	new Character("<span class=\"me\"><span>@</span></span>"),
-	new Character("<span>E</span>"),
-	new Character("<span>D</span>")
+	new Character("<span class=\"me\"><span>E</span></span>"),
+	new Character("<span class=\"me\"><span>D</span></span>")
 ];
 
 const player = characters[character.player];
 const enemies = [];
+let intervalId = null;
 
 function init() {
 	player.pos = new Vec2(4, 1);
 	initDots();
 	enemies[0] = characters[character.enemy1];
 	enemies[1] = characters[character.enemy2];
-	console.log(enemies);
+
 	enemies[0].pos = new Vec2(1, 4);
 	enemies[1].pos = new Vec2(7, 4);
+	for (let i = 0; i < enemies.length; i++) {
+		enemies[i].lastPos = new Vec2(enemies[i].pos.x, enemies[i].pos.y);
+	}
 
+	intervalId = setInterval(interval, 1000);
 	addEventListener("keydown", onKeyDown);
 	draw();
 }
@@ -73,18 +92,17 @@ function initDots() {
 }
 
 function onKeyDown(event) {
-	const targetPos = new Vec2(player.pos.x, player.pos.y);
-	console.log(cells[targetPos.y][targetPos.x]);
+	let targetPos = new Vec2(player.pos.x, player.pos.y);
 	switch (event.key) {
 		case "w":
-			if (targetPos.y > 0) {
+			// if (targetPos.y > 0) {
 				targetPos.y--;
-			}
+			// }
 			break;
 		case "s":
-			if (targetPos.y < cell.maxHeight) {
+			// if (targetPos.y < cell.maxHeight) {
 				targetPos.y++
-			}
+			// }
 			break;
 		case "a":
 			targetPos.x--;
@@ -94,12 +112,14 @@ function onKeyDown(event) {
 			break;
 	}
 
+	loopPos(targetPos);
+
 	if (cells[targetPos.y][targetPos.x] >= cell.max) {
 		return;
 	}
+
 	switch (cells[targetPos.y][targetPos.x]) {
 		case cell.none:
-			console.log(cell.none, cells[targetPos.y][targetPos.x]);
 			player.pos = new Vec2(targetPos.x, targetPos.y);
 		case cell.wall:
 			break;
@@ -111,6 +131,38 @@ function onKeyDown(event) {
 			console.log("No element found");
 	}
 	draw();
+}
+
+function interval() {
+	for (let i = 0; i < enemies.length; i++) {
+		enemyMove(enemies[i]);
+	}
+	draw();
+}
+
+function enemyMove(enemy) {
+	const pos = [];
+	let v = null;
+	console.log('yo');
+	for (let i = 0; i < Object.keys(direction).length; i++) {
+		v = new Vec2(enemy.pos.x + directions[i].x, enemy.pos.y + directions[i].y);
+		console.log(v);
+		if (cells[v.y][v.x] !== cell.wall) {
+			pos.push(v);
+		}
+	}
+	const r = Math.floor(Math.random() * pos.length);
+	enemy.pos = pos[r];
+}
+
+function loopPos(v) {
+	console.log("V", v);
+	if (v.x < 0) {
+		v.x = cells[v.y].length - 1;
+	}
+	if (v.x >= cells[v.y].length) {
+		v.x = 0;
+	}
 }
 
 function draw() {
