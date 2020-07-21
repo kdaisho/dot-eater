264a014473cd8aa8f21073c6032208d03b719138
daisho <daishokomiyama@gmail.com>
2020-07-21 15:07:07 -0400

    Replacing emoji with character

--- js/app.js
+++ js/app.js
@@ -58,10 +58,15 @@ function Character(aa) {
 	this.aa = aa;
 }
 
+// const characters = [
+// 	new Character("<span class=\"character\"><span>😎</span></span>"),
+// 	new Character("<span class=\"character\"><span>👹</span></span>"),
+// 	new Character("<span class=\"character\"><span>👺</span></span>")
+// ];
 const characters = [
-	new Character("<span class=\"character\"><span>😎</span></span>"),
-	new Character("<span class=\"character\"><span>👹</span></span>"),
-	new Character("<span class=\"character\"><span>👺</span></span>")
+	new Character("<span class=\"character\"><span>@</span></span>"),
+	new Character("<span class=\"character\"><span>T</span></span>"),
+	new Character("<span class=\"character\"><span>K</span></span>")
 ];
 
 const player = characters[character.player];
